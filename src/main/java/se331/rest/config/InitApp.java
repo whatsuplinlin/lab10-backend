192b12c3ce1b176a4b5520eee17fa7dfa6bf77fe
whatsuplinlin <kjm010704@gmail.com>
2022-09-22 13:09:52 +0700

    2.6

--- src/main/java/se331/rest/config/InitApp.java
+++ src/main/java/se331/rest/config/InitApp.java
@@ -9,6 +9,8 @@ import se331.rest.entity.Organizer;
 import se331.rest.repository.EventRepository;
 import se331.rest.repository.OrganizerRepository;
 
+import javax.transaction.Transactional;
+
 @Component
 public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     @Autowired
@@ -18,6 +20,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     OrganizerRepository organizerRepository;
 
     @Override
+    @Transactional
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
         Organizer org1,org2,org3;
         org1 = organizerRepository.save(Organizer.builder()
